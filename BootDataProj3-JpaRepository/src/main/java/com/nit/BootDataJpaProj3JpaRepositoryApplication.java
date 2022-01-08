aed6aa516ed7fe45a3250466c5cca4d58649fe22
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-08 14:01:34 +0530

    BootDataJpaProj3-JpaRepository

--- BootDataProj3-JpaRepository/src/main/java/com/nit/BootDataJpaProj3JpaRepositoryApplication.java
+++ BootDataProj3-JpaRepository/src/main/java/com/nit/BootDataJpaProj3JpaRepositoryApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataProj3JpaRepositoryApplication {
+public class BootDataJpaProj3JpaRepositoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataProj3JpaRepositoryApplication.class, args);
+		SpringApplication.run(BootDataJpaProj3JpaRepositoryApplication.class, args);
 	}
 }
