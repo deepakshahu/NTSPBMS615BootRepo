9feac219b7e11e36fce5e5f023b5d58283a29aa4
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-08 14:00:58 +0530

    BootDataJpaProj2-PASRepository

--- BootDataProj2-PASRepository/src/main/java/com/nit/BootDataJpaProj2PASRepositoryApplication.java
+++ BootDataProj2-PASRepository/src/main/java/com/nit/BootDataJpaProj2PASRepositoryApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataProj2PASRepositoryApplication {
+public class BootDataJpaProj2PASRepositoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataProj2PASRepositoryApplication.class, args);
+		SpringApplication.run(BootDataJpaProj2PASRepositoryApplication.class, args);
 	}
 }
