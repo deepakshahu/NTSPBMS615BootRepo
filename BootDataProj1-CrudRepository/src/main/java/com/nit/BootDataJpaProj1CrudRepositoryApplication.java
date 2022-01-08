949acbeb44184cf17a40fdd318134d90772cfafa
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-08 14:00:21 +0530

    BootDataJpaProj1-CrudRepository

--- BootDataProj1-CrudRepository/src/main/java/com/nit/BootDataJpaProj1CrudRepositoryApplication.java
+++ BootDataProj1-CrudRepository/src/main/java/com/nit/BootDataJpaProj1CrudRepositoryApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataProj1CrudRepositoryApplication {
+public class BootDataJpaProj1CrudRepositoryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataProj1CrudRepositoryApplication.class, args);
+		SpringApplication.run(BootDataJpaProj1CrudRepositoryApplication.class, args);
 	}
 }
