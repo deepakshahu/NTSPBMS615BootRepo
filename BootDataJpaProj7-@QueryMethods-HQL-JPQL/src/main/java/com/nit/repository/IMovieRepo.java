dfcc14670e77b1726e5f255a0f8d2feac63acff5
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-11 15:02:38 +0530

    BootDataJpaProj7-@QueryMethods-HQL-JPQL

--- BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nit/repository/IMovieRepo.java
+++ BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nit/repository/IMovieRepo.java
@@ -10,6 +10,7 @@ import com.nit.model.Movie;
 
 public interface IMovieRepo extends JpaRepository<Movie, Integer> {
 
+	//===========Entity Queries (Selecting all column values)==============
 	//@Query("from Movie where mid>=?1 and mid<=?2")
 	//@Query("from Movie as m where m.mid>=?1 and m.mid<=?2")  //m is alias name which is optional
 	//public List<Movie> searchMoviesByIdRange(int start, int end);
@@ -19,4 +20,18 @@ public interface IMovieRepo extends JpaRepository<Movie, Integer> {
 
 	@Query("from Movie where mid>=:min and mid<=:max")
 	public List<Movie> searchMoviesByIdRange(int min, int max);
+	
+	@Query("from Movie where mname In(:name1, :name2, :name3) order by mname asc")
+	public List<Movie> searchMoviesByMnames(String name1, String name2, String name3);  //taking array/var args/collection params for multiple
+	                                                            //(or)                                                             											    	//named params or positional params is not allowed
+	@Query("from Movie where mname In(?1,?2,?3) order by mname asc")
+	public List<Movie> searchMoviesByMnames1(String name1, String name2, String name3);
+	
+	//===========Scalar Queries(Selecting specific multiple column values)==============
+	@Query("select mid,mname,year from Movie where rating>=:rat and year in(:y1,:y2,:y3)")
+	public List<Object[]> fetchMovieDetailsByRatingsAndYear(float rat, String y1, String y2, String y3);
+	
+	//===========Scalar Queries (Selecting specific single column values)==============
+	@Query("select mname from Movie where year>=:start and year<=:end order by mname desc")
+	public List<String> fetchMoviesByYearRange(String start, String end);
 }
--- BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nit/runner/QueryMethodsTestRunner.java
+++ BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nit/runner/QueryMethodsTestRunner.java
@@ -17,8 +17,23 @@ public class QueryMethodsTestRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("===========Query methods execution==========");
-		List<Movie> list = repo.searchMoviesByIdRange(30, 50);
-		list.forEach(System.out::println);
+		System.out.println("===========Query methods execution(Selecting all column values)==========");
+		/*List<Movie> list = repo.searchMoviesByIdRange(30, 50);
+		list.forEach(System.out::println);*/
+		System.out.println("------------------------------------------");
+		repo.searchMoviesByMnames("RRR", "Don", "Baghi").forEach(System.out::println);
+		System.out.println("------------------------------------------");
+		repo.searchMoviesByMnames1("RRR", "Don", "Baghi").forEach(System.out::println);
+		
+		System.out.println("============Scalar Queries(Selecting specific multiple column values)============");
+		repo.fetchMovieDetailsByRatingsAndYear(4.0f, "2020", "2021", "2022").forEach(row->{
+			for(Object val:row)
+				System.out.print(val+" ");
+			
+			System.out.println();
+		});
+		
+		System.out.println("============Scalar Queries(Selecting specific single column values)============");
+		repo.fetchMoviesByYearRange("2020", "2022").forEach(System.out::println);
 	}
 }
