5a19085fbecedd1b5fd8e0a0341f9e86b6782ef6
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-02 15:16:00 +0530

    BootDataJpaProj08-@QueryMethods-NonSelect-HQL-JPQL

--- BootDataJpaProj8-@QueryMethods-NonSelect-HQL-JPQL/src/main/java/com/nit/BootDataJpaProj08QueryMethodsNonSelectHQLJPQLApplication.java
+++ BootDataJpaProj8-@QueryMethods-NonSelect-HQL-JPQL/src/main/java/com/nit/BootDataJpaProj08QueryMethodsNonSelectHQLJPQLApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataJpaProj8QueryMethodsNonSelectHQLJPQLApplication {
+public class BootDataJpaProj08QueryMethodsNonSelectHQLJPQLApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataJpaProj8QueryMethodsNonSelectHQLJPQLApplication.class, args);
+		SpringApplication.run(BootDataJpaProj08QueryMethodsNonSelectHQLJPQLApplication.class, args);
 	}
 }
