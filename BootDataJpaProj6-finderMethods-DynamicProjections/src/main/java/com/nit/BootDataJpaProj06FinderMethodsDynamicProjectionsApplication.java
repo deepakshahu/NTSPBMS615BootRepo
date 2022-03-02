6314b47073cbba56e9b82d798f9b1911830dac15
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-02 15:14:52 +0530

    BootDataJpaProj06-finderMethods-DynamicProjections

--- BootDataJpaProj6-finderMethods-DynamicProjections/src/main/java/com/nit/BootDataJpaProj06FinderMethodsDynamicProjectionsApplication.java
+++ BootDataJpaProj6-finderMethods-DynamicProjections/src/main/java/com/nit/BootDataJpaProj06FinderMethodsDynamicProjectionsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataJpaProj6FinderMethodsDynamicProjectionsApplication {
+public class BootDataJpaProj06FinderMethodsDynamicProjectionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataJpaProj6FinderMethodsDynamicProjectionsApplication.class, args);
+		SpringApplication.run(BootDataJpaProj06FinderMethodsDynamicProjectionsApplication.class, args);
 	}
 }
