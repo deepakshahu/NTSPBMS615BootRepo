acd9ee96871adf8deb00a7bc4e75b6bfd09ae2d7
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-02 15:15:35 +0530

    BootDataJpaProj07-@QueryMethods-HQL-JPQL

--- BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nit/BootDataJpaProj07QueryMethodsHQLJPQLApplication.java
+++ BootDataJpaProj7-@QueryMethods-HQL-JPQL/src/main/java/com/nit/BootDataJpaProj07QueryMethodsHQLJPQLApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataJpaProj7QueryMethodsHQLJPQLApplication {
+public class BootDataJpaProj07QueryMethodsHQLJPQLApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataJpaProj7QueryMethodsHQLJPQLApplication.class, args);
+		SpringApplication.run(BootDataJpaProj07QueryMethodsHQLJPQLApplication.class, args);
 	}
 }
