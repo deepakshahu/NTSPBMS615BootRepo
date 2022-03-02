96e9f2338e3e72855db6a40949bc058ef798cc0b
deepa <deepa@DESKTOP-AJQTNNR>
2022-03-02 15:16:31 +0530

    BootDataJpaProj09-@QueryMethods-NativeSQL

--- BootDataJpaProj9-@QueryMethods-NativeSQL/src/main/java/com/nit/BootDataJpaProj09QueryMethodsNativeSQLApplication.java
+++ BootDataJpaProj9-@QueryMethods-NativeSQL/src/main/java/com/nit/BootDataJpaProj09QueryMethodsNativeSQLApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootDataJpaProj9QueryMethodsNativeSQLApplication {
+public class BootDataJpaProj09QueryMethodsNativeSQLApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootDataJpaProj9QueryMethodsNativeSQLApplication.class, args);
+		SpringApplication.run(BootDataJpaProj09QueryMethodsNativeSQLApplication.class, args);
 	}
 }
