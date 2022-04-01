015c0db2806b4a4436573055fd564ace8093d55a
deepa <deepa@DESKTOP-AJQTNNR>
2022-04-01 12:57:06 +0530

    BootMVCProj08-MiniProject-CURDOperations-Improved

---
+++ BootMVCProj08-MiniProject-CURDOperations-Improved/src/main/java/com/nit/BootMvcProj08MiniProjectCurdOperationsImprovedApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj08MiniProjectCurdOperationsImprovedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj08MiniProjectCurdOperationsImprovedApplication.class, args);
+	}
+
+}
